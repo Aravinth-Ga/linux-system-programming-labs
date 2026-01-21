2d7e496213a66e50598683fa3d6d4674ff9af591
Aravinth Ganesan <aravintheng@gmail.com>
2026-01-21 21:42:43 +0100

    lab02-excercise

--- ch02-file-io/open-only/02.open_nonexistent_no_create.c
+++ ch02-file-io/open-only/02.open_nonexistent_no_create.c
@@ -1,3 +1,64 @@
 /*
     Lab 02: Open Non-Existent File without Create Flag
+
+    system calls used:
+        - open()
+        - close()
+    
+    Flags used:
+      - O_RDONLY
+
+    What this lab teaches:
+        - open() returns -1 when trying to open a non-existent file without O_CREAT flag.
+        - open does not create a new file if does not exist.
+        - close() should not be called if open() fails, since no valid file descriptor is allocated.
+
+    How to compile:
+        1. Ensure you are in the directory containing this source file.
+        2. Use the following command to compile:
+             gcc -Wall -Wextra -O2 -g 02_open_nonexistent_no_create.c -o lab02
+        3. Run the compiled program with a non-existent file:
+             ./lab02 nonexistent.txt
 */
+
+#include <fcntl.h>      // For open() and O_RDONLY
+#include <stdio.h>      // For printf() and perror()
+#include <unistd.h>     // For close()
+
+
+int main(int argc, char* argv[])
+{
+    const char* file_path = "data_lab02.txt";
+
+    if(argc == 1)
+    {
+        /* Do not change the file path, Use the predefined one*/
+    }
+    else if(argc == 2)
+    {
+        file_path = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+        return 1;
+    }
+
+    printf("Lab02: Attempting to open a non-existent file without O_CREAT flag.\n");
+
+    int fd = open(file_path, O_RDONLY);
+
+    if(fd < 0)
+    {
+        printf("The open() call returned value : %d on failure\n", fd);
+        perror("open");
+    }
+    
+    // This line should not be reached if its reached something worng with the test setup
+    printf("[Unexpected] Successfully opened file '%s' with FD: %d\n", file_path, fd);
+    // Close the file descriptor if the file is opened successfully
+    close(fd);
+
+    return 0;
+
+}
