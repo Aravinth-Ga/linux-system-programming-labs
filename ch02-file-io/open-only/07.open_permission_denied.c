0e6949add3876721c2cb728cbcaaea30962d03ef
Aravinth Ganesan <aravintheng@gmail.com>
2026-01-26 20:13:31 +0100

    Repo changes and lab07 excerceise

--- ch02-file-io/open-only/07.open_permission_denied.c
+++ ch02-file-io/open-only/07.open_permission_denied.c
@@ -8,7 +8,33 @@
     
     Flags used:
         O_WRONLY    (Open file in write-only mode)
-        O_CREAT     (Create file if it does not exist)
-        O_EXCL      (Ensure that this call creates the file)
+        O_RDONLY    (Open file in read-only mode)
+        O_RDWR    (Open file in read-write mode)
+
+    Error handled:
+        Permission denied
     
+    What this lab teaches:
+        how the system behaves when opening a file without sufficient permissions.
 */
+
+#include <fcntl.h>      // For open(), O_RDONLY, O_WRONLY
+#include <unistd.h>     // close()
+#include <stdio.h>      // For printf(), perror()
+
+
+
+/*  Main entry point of the program
+
+    argc : Argument count
+    argv : Argument vector (array of strings)
+
+    Return : 0 on success and non-zero on error
+*/
+
+int main(int argc, char* argv[])
+{
+    const char* file = "lab07_secret.txt";
+
+    
+}
